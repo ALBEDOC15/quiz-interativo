84f4fddd479363266eb25d9c2ea218f105a9924b
Henrique Teixeira de Oliveira <mac0156483@aluno.firjansenaisesi.com.br>
2025-12-12 19:53:55 -0300

    versao inicial do quiz

---
+++ sexta-feira.js
@@ -0,0 +1,41 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const quiz = [
+    {
+    pergunta: "quem descobriu o Brasil? ",
+    opçoes:["Colombo", "Prinecesa Isabel","Getulio", "Cabral"],
+    resposta: 3
+}
+]
+
+let score = 0;
+
+function fazerPergunta(index) {
+    if (index >= quiz.length){
+        console.log(`Quiz concluido. voce acertou ${score} de ${quiz.length} perguntas.`);
+    rl.close();
+    return;
+    }
+    const pergunta = quiz[index];
+    console.log(`\npergunta ${index +1}: ${pergunta.pergunta}`);
+    pergunta.opçoes.forEach((opçao, i) => {
+        console.log(`${i +1}. ${opçao}`);
+    });
+
+    rl.question("sua resposta: ", (answer) => {
+        const resposta = parseInt(answer) -1;
+        if (resposta === pergunta.resposta){
+            score++;
+            console.log("correto! Genio!!!!");
+        } else{
+            console.log(`Errouuuuu! A resposta certa era: ${pergunta.opçoes[pergunta.resposta]}`)
+        }
+        fazerPergunta(index + 1)
+    })
+}
+fazerPergunta(0);
